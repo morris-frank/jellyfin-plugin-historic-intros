45da956de4c9413967860422d1c8f283dfb4d880
Maurice Frank <maurice.frank@posteo.de>
2024-02-19 22:12:27 +0100

    different config

--- Jellyfin.Plugin.HistoricIntros/API/HistoricIntrosController.cs
+++ Jellyfin.Plugin.HistoricIntros/API/HistoricIntrosController.cs
@@ -32,7 +32,97 @@ public class HistoricIntrosController : ControllerBase
         this.logger = loggerFactory.CreateLogger<HistoricIntrosController>();
     }
 
+    [HttpPost("LoadIntros")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult LoadIntros()
+    {
+        logger.LogDebug("Loading Intros");
+        PopulateIntroLibrary();
+
+        var inLibrary = HistoricIntrosPlugin.LibraryManager.GetItemsResult(new InternalItemsQuery
+        {
+            Tags = new[] { "intros.video" },
+            HasAnyProviderId = new Dictionary<string, string>
+            {
+                {"intros.trailers.video", ""},
+                {"intros.prerolls.video", ""}
+            }
+        }).Items.ToDictionary(x => x.Path, x => x);
+        foreach (var item in inLibrary)
+        {
+            logger.LogDebug("Found {0}", item.Value.Name);
+        }
+        return Ok();
+    }
+
     private static string trailersPath => HistoricIntrosPlugin.Instance.Configuration.TrailersPath;
     private static string prerollsPath => HistoricIntrosPlugin.Instance.Configuration.PrerollsPath;
     private static int numberOfTrailers => HistoricIntrosPlugin.Instance.Configuration.NumberOfTrailers;
+
+    private void PopulateIntroLibrary()
+    {
+        var inLibrary = HistoricIntrosPlugin.LibraryManager.GetItemsResult(new InternalItemsQuery
+        {
+            Tags = new[] { "intros.video" },
+            HasAnyProviderId = new Dictionary<string, string>
+            {
+                {"intros.trailers.video", ""},
+                {"intros.prerolls.video", ""}
+            }
+        }).Items.ToDictionary(x => x.Path, x => x);
+
+        var trailerFolders = Directory.GetDirectories(trailersPath);
+        foreach (var folder in trailerFolders)
+        {
+            var year = Path.GetFileName(folder);
+            var trailers = Directory.GetFiles(folder);
+            foreach (var path in trailers)
+            {
+                if (inLibrary.ContainsKey(path) || path.StartsWith("."))
+                {
+                    continue;
+                }
+
+                var item = new Video
+                {
+                    Id = Guid.NewGuid(),
+                    Name = Path.GetFileNameWithoutExtension(path),
+                    Path = path,
+                    ProductionYear = int.Parse(year),
+                    ProviderIds = new Dictionary<string, string>
+                    {
+                        {"intros.trailers.video", ""}
+                    },
+                };
+                logger.LogDebug("Creating trailer {0}", item.Name);
+                HistoricIntrosPlugin.LibraryManager.CreateItem(item, null);
+            }
+        }
+
+        var prerolls = Directory.GetFiles(prerollsPath, "*.*", SearchOption.AllDirectories);
+        foreach (var path in prerolls)
+        {
+            if (inLibrary.ContainsKey(path) || path.StartsWith("."))
+            {
+                continue;
+            }
+
+            var item = new Video
+            {
+                Id = Guid.NewGuid(),
+                Name = Path.GetFileNameWithoutExtension(path),
+                Path = path,
+                ProviderIds = new Dictionary<string, string>
+                {
+                    {"intros.prerolls.video", ""}
+                },
+            };
+            logger.LogDebug("Creating preroll {0}", item.Name);
+            HistoricIntrosPlugin.LibraryManager.CreateItem(item, null);
+        }
+
+
+    }
+
 }
